f79bb35beba3c441296006cff9fed0fd6d7b5268
Tri Ngo <tringo831@gmail.com>
2024-12-17 15:27:06 -0800

    Add protected routes with role-based authorization

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthProvider'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>    
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>    
   </StrictMode>,
 )
