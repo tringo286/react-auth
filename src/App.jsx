299ce50d26e8581bb872112896a6d5588c80baf7
Tri Ngo <tringo831@gmail.com>
2024-12-16 19:27:16 -0800

    Add login with err handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Register from './Register'
+import Login from './Login'
 
 const App = () => {
   return (
     <main className='App'>
-      <Register />
+      <Login />
     </main>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AuthContext from './context/AuthProvider.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthContext>
+      <App />
+    </AuthContext>    
   </StrictMode>,
 )
