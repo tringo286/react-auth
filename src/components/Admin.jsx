131b350fcebf719e2faee60a787c6bff7625dc20
Tri Ngo <tringo831@gmail.com>
2024-12-17 17:42:22 -0800

    Implement JWT authentication with access/refresh tokens, cookies, and Axios

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom"
+import Users from "./User"
 
 const Admin = () => {
     return (
         <section>
             <h1>Admins Page</h1>
+            <br />
+            <Users />
             <br />
             <p>You must have been assigned an Admin role.</p>
             <div className="flexGrow">
